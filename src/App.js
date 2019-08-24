ed54a3607ef0185fcdbbe1fb447895d33add588b
Akbar Karshiev <akbar.karshiev.97@gmail.com>
2019-08-24 15:04:39 +0500

    3.20.Class Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 },
+    ],
+    otherState: 'some other value'
+  }
+
+  switchNameHandler = () => {
+    //console.log("Was clicked");
+    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+    this.setState({
+      persons: [
+        { name: 'Maximilian', age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 },
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
        <h1>Hello world</h1>
+       <button onClick={this.switchNameHandler}>Switch Name</button>
+       <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+       <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
+       <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
   }
